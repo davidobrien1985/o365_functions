b0352a25e0a3c902b2d0c2096cc9c9b82fda5b87
David OBrien <me@david-obrien.net>
2017-03-23 16:41:54 +1100

    Check if function is called from allowed Slack channel

--- o365_compiled/run.cs
+++ o365_compiled/run.cs
@@ -14,12 +14,6 @@ namespace o365_compiled
         {
             log.Info($"C# HTTP trigger function processed a request. RequestUri={req.RequestUri}");
 
-            string jsonContent = await req.Content.ReadAsStringAsync();
-            log.Info(jsonContent);
-            // assign the Slack payload "text" to be the UPN of the user that needs the license
-            string username = (jsonContent.Split('&')[8]).Split('=')[1];
-            log.Info(username);
-            
             JArray skus = null;
             string skuId = null;
             string e3SkuId = null;
@@ -28,46 +22,62 @@ namespace o365_compiled
             string clientId = GetEnvironmentVariable("clientId");
             string clientSecret = GetEnvironmentVariable("clientSecret");
             string tenantId = GetEnvironmentVariable("tenantId");
+            string allowedChannelName = GetEnvironmentVariable("allowedChannelName");
             string res = null;
             JObject e3SkuObject = new JObject();
             JObject e1SkuObject = new JObject();
 
-            // acquire Bearer Token for AD Application user through Graph API
-            string token = AuthenticationHelperRest.AcquireTokenBySpn(tenantId, clientId, clientSecret);
-            string bearerToken = "Bearer " + token;
+            string jsonContent = await req.Content.ReadAsStringAsync();
+            log.Info(jsonContent);
+            // assign the Slack payload "text" to be the UPN of the user that needs the license
+            string username = (jsonContent.Split('&')[8]).Split('=')[1];
+            log.Info(username);
 
-            log.Info("Getting License SKUs...");
-            // get information about all the O365 SKUs available
-            skus = LicensingHelper.GetO365Skus(graphApiVersion, bearerToken);
-            e1SkuObject = SubscriptionHelper.GetSkuId(skus, "STANDARDPACK");
-            e3SkuObject = SubscriptionHelper.GetSkuId(skus, "ENTERPRISEPACK");
-
-            e1SkuId = (string) e1SkuObject["skuId"];
-            e3SkuId = (string) e3SkuObject["skuId"];
-
-            int usedLicenses = e3SkuObject.GetValue("consumedUnits").Value<int>();
-            int purchasedLicenses = e3SkuObject.SelectToken(@"prepaidUnits.enabled").Value<int>();
-
-            // check if enough licenses available
-            if (usedLicenses < purchasedLicenses)
+            // assign the Slack payload "channel_name" to the allowed channel name for this code to be called from
+            string channelName = (jsonContent.Split('&')[4]).Split('=')[1];
+            if (channelName == allowedChannelName)
             {
-                // enough licenses, so do it
-                log.Info("Setting License...");
-                string returnedUserName =
-                    LicensingHelper.SetO365LicensingInfo(graphApiVersion, bearerToken, username, e3SkuId, e1SkuId);
-                res =
-                    $"There are {purchasedLicenses} available E3 licenses and {usedLicenses} already used. You have just used one more." +
-                    $"Successfully assigned license to {returnedUserName}";
+                // acquire Bearer Token for AD Application user through Graph API
+                string token = AuthenticationHelperRest.AcquireTokenBySpn(tenantId, clientId, clientSecret);
+                string bearerToken = "Bearer " + token;
+
+                log.Info("Getting License SKUs...");
+                // get information about all the O365 SKUs available
+                skus = LicensingHelper.GetO365Skus(graphApiVersion, bearerToken);
+                e1SkuObject = SubscriptionHelper.GetSkuId(skus, "STANDARDPACK");
+                e3SkuObject = SubscriptionHelper.GetSkuId(skus, "ENTERPRISEPACK");
+
+                e1SkuId = (string) e1SkuObject["skuId"];
+                e3SkuId = (string) e3SkuObject["skuId"];
+
+                int usedLicenses = e3SkuObject.GetValue("consumedUnits").Value<int>();
+                int purchasedLicenses = e3SkuObject.SelectToken(@"prepaidUnits.enabled").Value<int>();
+
+                // check if enough licenses available
+                if (usedLicenses < purchasedLicenses)
+                {
+                    // enough licenses, so do it
+                    log.Info("Setting License...");
+                    string returnedUserName =
+                        LicensingHelper.SetO365LicensingInfo(graphApiVersion, bearerToken, username, e3SkuId, e1SkuId);
+                    res =
+                        $"There are {purchasedLicenses} available E3 licenses and {usedLicenses} already used. You have just used one more." +
+                        $"Successfully assigned license to {returnedUserName}";
+                }
+                else
+                {
+                    // not enough licenses, notify user
+                    log.Info(
+                        $"There are {purchasedLicenses} available E3 licenses and {usedLicenses} already used. No licenses available for E3. Please log on to portal.office.com and buy new licenses.");
+                    res =
+                        $"There are {purchasedLicenses} available E3 licenses and {usedLicenses} already used. No licenses available for E3. Please log on to portal.office.com and buy new licenses.";
+                }
             }
             else
             {
-                // not enough licenses, notify user
-                log.Info(
-                    $"There are {purchasedLicenses} available E3 licenses and {usedLicenses} already used. No licenses available for E3. Please log on to portal.office.com and buy new licenses.");
                 res =
-                    $"There are {purchasedLicenses} available E3 licenses and {usedLicenses} already used. No licenses available for E3. Please log on to portal.office.com and buy new licenses.";
+                    $"The channel {channelName} is not allowed to execute this function. Sorry. Get in touch with your O365 admin.";
             }
-
             return res;
         }
 
