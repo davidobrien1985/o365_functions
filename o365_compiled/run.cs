0674acc7ffe8720729d872977a0fbf8e10150acc
David OBrien <me@david-obrien.net>
2017-03-24 12:07:59 +1100

    add GetLicenseInfo function to app

--- o365_compiled/run.cs
+++ o365_compiled/run.cs
@@ -126,8 +126,8 @@ namespace o365_compiled
                 e1SkuObject = SubscriptionHelper.FilterSkusByPartNumber(skus, "STANDARDPACK");
                 e3SkuObject = SubscriptionHelper.FilterSkusByPartNumber(skus, "ENTERPRISEPACK");
 
-                e1SkuId = (string)e1SkuObject["skuId"];
-                e3SkuId = (string)e3SkuObject["skuId"];
+                e1SkuId = (string) e1SkuObject["skuId"];
+                e3SkuId = (string) e3SkuObject["skuId"];
 
                 int usedLicenses = e1SkuObject.GetValue("consumedUnits").Value<int>();
                 int purchasedLicenses = e1SkuObject.SelectToken(@"prepaidUnits.enabled").Value<int>();
@@ -154,6 +154,45 @@ namespace o365_compiled
             }
 
             return res;
+        }
+
+        public static string GetEnvironmentVariable(string name)
+        {
+            return System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+        }
+    }
+
+    public class GetLicenseInfo
+    {
+        public static async Task<object> Run(HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info($"C# HTTP trigger function processed a request.RequestURI= {req.RequestUri}");
+            double graphApiVersion = double.Parse(GetEnvironmentVariable("graphApiVersion"));
+            string clientId = GetEnvironmentVariable("clientId");
+            string clientSecret = GetEnvironmentVariable("clientSecret");
+            string tenantId = GetEnvironmentVariable("tenantId");
+            string allowedChannelName = GetEnvironmentVariable("allowedChannelName");
+            string res = null;
+
+            string jsonContent = await req.Content.ReadAsStringAsync();
+            log.Info(jsonContent);
+            // assign the Slack payload "text" to be the UPN of the user that needs the license
+            string username = (jsonContent.Split('&')[8]).Split('=')[1];
+            log.Info(username);
+
+            // assign the Slack payload "channel_name" to the allowed channel name for this code to be called from
+            string channelName = (jsonContent.Split('&')[4]).Split('=')[1];
+            if (channelName == allowedChannelName)
+            {
+                // acquire Bearer Token for AD Application user through Graph API
+                string token = AuthenticationHelperRest.AcquireTokenBySpn(tenantId, clientId, clientSecret);
+                string bearerToken = "Bearer " + token;
+                string skuPartNumber = LicensingHelper.GetUserLicenseInfo(graphApiVersion, username, bearerToken);
+
+                res = $"{username} is licensed with the {skuPartNumber} license.";
+            }
+            return res;
+
         }
 
         public static string GetEnvironmentVariable(string name)
