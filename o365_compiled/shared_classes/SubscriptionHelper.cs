0daabc6aa096e6d6ab7fd10034bd37a0d1776804
David OBrien <me@david-obrien.net>
2017-03-24 12:07:18 +1100

    add FilterSkusBySkuId method to SubscriptionHelper class

--- o365_compiled/shared_classes/SubscriptionHelper.cs
+++ o365_compiled/shared_classes/SubscriptionHelper.cs
@@ -22,5 +22,20 @@ namespace o365_compiled.shared_classes
             }
             return new JObject();
         }
+
+        public static JObject FilterSkusBySkuId(JArray skus, string skuId)
+        {
+            foreach (JToken sku in skus)
+            {
+                JObject skuObject = (JObject)sku;
+
+                if ((string)skuObject["skuId"] == skuId)
+                {
+                    skuObject = (JObject)sku;
+                    return skuObject;
+                }
+            }
+            return new JObject();
+        }
     }
 }
