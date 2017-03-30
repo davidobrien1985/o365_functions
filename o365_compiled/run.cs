a0d2f716b2855cdd5823f5cf726ec1f23b44e782
David OBrien <me@david-obrien.net>
2017-03-30 12:23:33 +1100

    Refactor to write to multiple queues to trigger right function

--- o365_compiled/run.cs
+++ o365_compiled/run.cs
@@ -25,7 +25,7 @@ namespace o365_compiled
 
     public class run
     {
-        public static async Task<string> Run(HttpRequestMessage req, TraceWriter log, IAsyncCollector<payload> outputQueue)
+        public static async Task<string> Run(HttpRequestMessage req, TraceWriter log, IAsyncCollector<payload> allocatee3o365, IAsyncCollector<payload> geto365userlicense, IAsyncCollector<payload> deallocatee3o365)
         {
             log.Info($"C# HTTP trigger function processed a request. Command used={req.RequestUri}");
 
@@ -38,6 +38,7 @@ namespace o365_compiled
             string channelName = (jsonContent.Split('&')[4]).Split('=')[1];
             if (channelName == allowedChannelName)
             {
+                
                 payload json = new payload
                 {
                     Token = (jsonContent.Split('&')[0]).Split('=')[1],
@@ -51,8 +52,24 @@ namespace o365_compiled
                     Text = (jsonContent.Split('&')[8]).Split('=')[1],
                     Response_Url = (jsonContent.Split('&')[9]).Split('=')[1]
                 };
+                string command = Uri.EscapeDataString(json.Command);
 
-                await outputQueue.AddAsync(json);
+                if (command == "%252Fallocatee3o365")
+                {
+                    // add to allocateE3O365 queue
+                    await allocatee3o365.AddAsync(json);
+                }
+                else if (command == "%252Fdeallocatee3o365")
+                {
+                    // add to deallocatee3o365 queue
+                    await deallocatee3o365.AddAsync(json);
+                }
+                else if (command == "%252Fgeto365userlicense")
+                {
+                    // add to geto365userlicense queue
+                    await geto365userlicense.AddAsync(json);
+                }
+                
                 res = $"Hey, {json.User_Name}, I'm working on assigning the license. I'll let you know when I'm done...";
             }
             return res;
@@ -60,92 +77,79 @@ namespace o365_compiled
     }
 
     
-    public class ComputeJobFromQueue
+    public class AllocateE3License
     {
-        public static void Run(payload myQueueItem, TraceWriter log)
+        public static void Run(payload allocatee3o365, TraceWriter log)
         {
-            log.Info($"C# Queue trigger function processed: {myQueueItem.User_Name}");
+            log.Info($"C# Queue trigger function processed: {allocatee3o365.User_Name}");
             double graphApiVersion = double.Parse(GenericHelper.GetEnvironmentVariable("graphApiVersion"));
             string clientId = GenericHelper.GetEnvironmentVariable("clientId");
             string clientSecret = GenericHelper.GetEnvironmentVariable("clientSecret");
             string tenantId = GenericHelper.GetEnvironmentVariable("tenantId");
 
             // assign the Slack payload "text" to be the UPN of the user that needs the license
-            string username = myQueueItem.Text;
+            string username = allocatee3o365.Text;
             log.Info(username);
-            string command = Uri.EscapeDataString(myQueueItem.Command);
-            log.Info(command);
-            if (command == "%252Fallocatee3o365")
+
+            // acquire Bearer Token for AD Application user through Graph API
+            string token = AuthenticationHelperRest.AcquireTokenBySpn(tenantId, clientId, clientSecret);
+            string bearerToken = "Bearer " + token;
+
+            log.Info("Getting License SKUs...");
+            // get information about all the O365 SKUs available
+            JArray skus = LicensingHelper.GetO365Skus(graphApiVersion, bearerToken);
+            JObject e1SkuObject = SubscriptionHelper.FilterSkusByPartNumber(skus, "STANDARDPACK");
+            JObject e3SkuObject = SubscriptionHelper.FilterSkusByPartNumber(skus, "ENTERPRISEPACK");
+
+            string e1SkuId = (string) e1SkuObject["skuId"];
+            string e3SkuId = (string) e3SkuObject["skuId"];
+
+            int usedLicenses = e3SkuObject.GetValue("consumedUnits").Value<int>();
+            int purchasedLicenses = e3SkuObject.SelectToken(@"prepaidUnits.enabled").Value<int>();
+
+            // check if enough licenses available
+            if (usedLicenses < purchasedLicenses)
             {
+                // enough licenses, so do it
+                log.Info("Setting License...");
+                string returnedUserName =
+                    LicensingHelper.SetO365LicensingInfo(graphApiVersion, bearerToken, username, e3SkuId, e1SkuId);
 
-                // acquire Bearer Token for AD Application user through Graph API
-                string token = AuthenticationHelperRest.AcquireTokenBySpn(tenantId, clientId, clientSecret);
-                string bearerToken = "Bearer " + token;
+                var uri = Uri.UnescapeDataString(allocatee3o365.Response_Url);
 
-                log.Info("Getting License SKUs...");
-                // get information about all the O365 SKUs available
-                JArray skus = LicensingHelper.GetO365Skus(graphApiVersion, bearerToken);
-                JObject e1SkuObject = SubscriptionHelper.FilterSkusByPartNumber(skus, "STANDARDPACK");
-                JObject e3SkuObject = SubscriptionHelper.FilterSkusByPartNumber(skus, "ENTERPRISEPACK");
-
-                string e1SkuId = (string) e1SkuObject["skuId"];
-                string e3SkuId = (string) e3SkuObject["skuId"];
-
-                int usedLicenses = e3SkuObject.GetValue("consumedUnits").Value<int>();
-                int purchasedLicenses = e3SkuObject.SelectToken(@"prepaidUnits.enabled").Value<int>();
-
-                // check if enough licenses available
-                if (usedLicenses < purchasedLicenses)
+                var jsonPayload = new
                 {
-                    // enough licenses, so do it
-                    log.Info("Setting License...");
-                    string returnedUserName =
-                        LicensingHelper.SetO365LicensingInfo(graphApiVersion, bearerToken, username, e3SkuId, e1SkuId);
+                    text =
+                    $"There are {purchasedLicenses} available E3 licenses and {usedLicenses} already used. You have just used one more. Successfully assigned *E3* license to {returnedUserName}"
+                };
 
-                    var uri = Uri.UnescapeDataString(myQueueItem.Response_Url);
+                GenericHelper.SendMessageToSlack(uri, jsonPayload);
+            }
+            else
+            {
+                // not enough licenses, notify user
+                log.Info(
+                    $"There are {purchasedLicenses} available E3 licenses and {usedLicenses} already used. No licenses available for E3. Please log on to portal.office.com and buy new licenses.");
+                var uri = Uri.UnescapeDataString(allocatee3o365.Response_Url);
 
-                    var jsonPayload = new
-                    {
-                        text =
-                        $"There are {purchasedLicenses} available E3 licenses and {usedLicenses} already used. You have just used one more. Successfully assigned *E3* license to {returnedUserName}"
-                    };
-
-                    GenericHelper.SendMessageToSlack(uri, jsonPayload);
-                }
-                else
+                log.Info(uri);
+                var jsonPayload = new
                 {
-                    // not enough licenses, notify user
-                    log.Info(
-                        $"There are {purchasedLicenses} available E3 licenses and {usedLicenses} already used. No licenses available for E3. Please log on to portal.office.com and buy new licenses.");
-                    var uri = Uri.UnescapeDataString(myQueueItem.Response_Url);
+                    text =
+                    $"There are {purchasedLicenses} available E3 licenses and {usedLicenses} already used. No licenses available for E3. Please log on to portal.office.com and buy new licenses."
+                };
 
-                    log.Info(uri);
-                    var jsonPayload = new
-                    {
-                        text =
-                        $"There are {purchasedLicenses} available E3 licenses and {usedLicenses} already used. No licenses available for E3. Please log on to portal.office.com and buy new licenses."
-                    };
-
-                    GenericHelper.SendMessageToSlack(uri, jsonPayload);
-                }
-            }
-            else if (command == "%252Fdeallocatee3o365")
-            {
-                Deallocatelicense.Run(myQueueItem, log);
-            }
-            else if (command == "%252Fgeto365userlicense")
-            {
-                GetLicenseInfo.Run(myQueueItem, log);
+                GenericHelper.SendMessageToSlack(uri, jsonPayload);
             }
         }
     }
 
     public class Deallocatelicense
     {
-        public static void Run(payload req, TraceWriter log)
+        public static void Run(payload deallocatee3o365, TraceWriter log)
         {
 
-            log.Info($"C# HTTP trigger function processed a request. Command used={req.Command}");
+            log.Info($"C# HTTP trigger function processed a request. Command used={deallocatee3o365.Command}");
             double graphApiVersion = double.Parse(GenericHelper.GetEnvironmentVariable("graphApiVersion"));
             string clientId = GenericHelper.GetEnvironmentVariable("clientId");
             string clientSecret = GenericHelper.GetEnvironmentVariable("clientSecret");
@@ -153,12 +157,12 @@ namespace o365_compiled
             string allowedChannelName = GenericHelper.GetEnvironmentVariable("allowedChannelName");
 
             // assign the Slack payload "text" to be the UPN of the user that needs the license
-            string username = req.Text;
+            string username = deallocatee3o365.Text;
             log.Info(username);
 
             // assign the Slack payload "channel_name" to the allowed channel name for this code to be called from
-            string channelName = req.Channel_Name;
-            if (channelName == allowedChannelName && req.User_Name == "david")
+            string channelName = deallocatee3o365.Channel_Name;
+            if (channelName == allowedChannelName && deallocatee3o365.User_Name == "david")
             {
                 // acquire Bearer Token for AD Application user through Graph API
                 string token = AuthenticationHelperRest.AcquireTokenBySpn(tenantId, clientId, clientSecret);
@@ -184,7 +188,7 @@ namespace o365_compiled
                         LicensingHelper.SetO365LicensingInfo(graphApiVersion, bearerToken, username, e1SkuId,
                             e3SkuId);
 
-                    var uri = Uri.UnescapeDataString(req.Response_Url);
+                    var uri = Uri.UnescapeDataString(deallocatee3o365.Response_Url);
                     var jsonPayload = new
                     {
                         text =
@@ -200,7 +204,7 @@ namespace o365_compiled
                     log.Info(
                         $"There are {purchasedLicenses} available E1 licenses and {usedLicenses} already used. No licenses available for E1. Please log on to portal.office.com and buy new licenses.");
                     
-                    var uri = Uri.UnescapeDataString(req.Response_Url);
+                    var uri = Uri.UnescapeDataString(deallocatee3o365.Response_Url);
                     var jsonPayload = new
                     {
                         text =
@@ -215,9 +219,9 @@ namespace o365_compiled
 
     public class GetLicenseInfo
     {
-        public static void Run(payload req, TraceWriter log)
+        public static void Run(payload geto365userlicense, TraceWriter log)
         {
-            log.Info($"C# HTTP trigger function processed a request. Command used={req.Command}");
+            log.Info($"C# HTTP trigger function processed a request. Command used={geto365userlicense.Command}");
             double graphApiVersion = double.Parse(GenericHelper.GetEnvironmentVariable("graphApiVersion"));
             string clientId = GenericHelper.GetEnvironmentVariable("clientId");
             string clientSecret = GenericHelper.GetEnvironmentVariable("clientSecret");
@@ -225,12 +229,12 @@ namespace o365_compiled
             string allowedChannelName = GenericHelper.GetEnvironmentVariable("allowedChannelName");
 
             // assign the Slack payload "text" to be the UPN of the user that needs the license
-            string username = req.Text;
+            string username = geto365userlicense.Text;
             string encUserName = Uri.UnescapeDataString(username);
 
             log.Info(encUserName);
             // assign the Slack payload "channel_name" to the allowed channel name for this code to be called from
-            string channelName = req.Channel_Name;
+            string channelName = geto365userlicense.Channel_Name;
             if (channelName == allowedChannelName)
             {
                 // acquire Bearer Token for AD Application user through Graph API
@@ -239,7 +243,7 @@ namespace o365_compiled
                 string skuPartNumber =
                     LicensingHelper.GetUserLicenseInfo(graphApiVersion, encUserName, bearerToken, log);
 
-                var uri = Uri.UnescapeDataString(req.Response_Url);
+                var uri = Uri.UnescapeDataString(geto365userlicense.Response_Url);
                 var jsonPayload = new
                 {
                     text = $"{encUserName} is licensed with the {skuPartNumber} license."
